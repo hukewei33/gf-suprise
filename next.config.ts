56939ebd6e80482855b026a9406e27dba07b594b
kenny mac <hukewei33@gmail.com>
2025-11-22 15:57:39 -0500

    remove bad ci

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/gf-suprise',
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
